66c711809b18dc36f7d7300c8c2d3b70b995935d
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-06-10 02:42:23 +0530

    Setup vite pwa and add manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from 'vite-plugin-pwa';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
       react(),
-    tailwindcss()
+    tailwindcss(),
+      VitePWA({
+          registerType: 'autoUpdate',
+          manifest: {
+              name: 'Bike Parts POS',
+              short_name: 'BikePOS',
+              theme_color: '#1e40af'
+          }
+      })
   ],
 })
