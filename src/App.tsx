31a02f4c4be963e272a0bd890d3e76eaf9008b96
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-06-12 02:12:43 +0530

    Update the app.tsx to check the database connectivity

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
+import DBTest from "./db/DBTest.tsx";
 
 function App() {
 
   return (
+      <div>
+          <h1>Database Test</h1>
+          <DBTest />
+      </div>
 
-    <>
-      <h1 className="text-3xl bg-amber-200">Hello POS</h1>
-    </>
+    // <>
+    //   <h1 className="text-3xl bg-amber-200">Hello POS</h1>
+    // </>
   )
 }
 
