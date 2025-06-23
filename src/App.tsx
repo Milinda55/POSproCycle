78089e89bc05b9a746bca40b56e53a72da0a7185
Milinda Karunaratne <milindakarunaratna55@gmail.com>
2025-06-23 14:55:53 +0530

    create styles for inventory and create testing helpers

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,17 @@ import { initDB } from './db/initDB.ts';
 (window as any).initDB = initDB;
 
 import { SyncManager } from './db/sync.ts';
+import {useEffect} from "react";
+import {initTestingHelpers} from "./utils/testingHelpers.ts";
 (window as any).SyncManager = SyncManager;
 
 function App() {
+
+    useEffect(() => {
+        if (process.env.NODE_ENV === 'development') {
+            initTestingHelpers();
+        }
+    }, []);
 
   return (
 
